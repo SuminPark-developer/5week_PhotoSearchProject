f138e8af7929013604ca51ed77fa0d60e3738bbb
SuminPark-developer <float@kakao.com>
2021-10-13 01:20:26 +0900

    소셜 로그인 - 카카오 로그인, 네이버 로그인 구현 완료.

--- 5week_PhotoSearchProject/AppDelegate.swift
+++ 5week_PhotoSearchProject/AppDelegate.swift
@@ -7,15 +7,53 @@
 
 import UIKit
 import CoreData
+import KakaoSDKCommon
+import KakaoSDKAuth
+import NaverThirdPartyLogin
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        KakaoSDKCommon.initSDK(appKey: "bc62f55e3951a7b5a7ac6fd0fcb845b5")
+        
+        let instance = NaverThirdPartyLoginConnection.getSharedInstance()
+        // 네이버 앱으로 인증하는 방식을 활성화
+        instance?.isNaverAppOauthEnable = true
+        
+        // SafariViewController에서 인증하는 방식을 활성화
+        instance?.isInAppOauthEnable = true
+        
+        // 인증 화면을 iPhone의 세로 모드에서만 사용하기
+        instance?.isOnlyPortraitSupportedInIphone()
+        
+        // 네이버 아이디로 로그인하기 설정
+        // https://developers.naver.com/forum/posts/29572
+        // 애플리케이션을 등록할 때 입력한 URL Scheme
+//        instance?.serviceUrlScheme = kServiceAppUrlScheme
+        instance?.serviceUrlScheme = "naverlogin"
+        // 애플리케이션 등록 후 발급받은 클라이언트 아이디
+//        instance?.consumerKey = kConsumerKey
+        instance?.consumerKey = "HE2cZTM_ybqiH0MZwjLy"
+        // 애플리케이션 등록 후 발급받은 클라이언트 시크릿
+//        instance?.consumerSecret = kConsumerSecret
+        instance?.consumerSecret = "tsS8Cg3iS1"
+        // 애플리케이션 이름
+//        instance?.appName = kServiceAppName
+        instance?.appName = "Photo Search Project"
+        
         return true
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        if (AuthApi.isKakaoTalkLoginUrl(url)) {
+            return AuthController.handleOpenUrl(url: url)
+        }
+        
+        NaverThirdPartyLoginConnection.getSharedInstance()?.application(app, open: url, options: options)
+
+        return false
     }
 
     // MARK: UISceneSession Lifecycle
--- 5week_PhotoSearchProject/ViewController.swift
+++ 5week_PhotoSearchProject/ViewController.swift
@@ -6,14 +6,107 @@
 //
 
 import UIKit
+import KakaoSDKAuth
+import KakaoSDKUser
 
-class ViewController: UIViewController {
+import Alamofire
+import NaverThirdPartyLogin
 
+class ViewController: UIViewController, NaverThirdPartyLoginConnectionDelegate {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var id: UILabel!
+    
+    let loginInstance = NaverThirdPartyLoginConnection.getSharedInstance()
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//        super.viewDidLoad()
+        loginInstance?.delegate = self
     }
 
 
+    @IBAction func didTapKakaoLoginBtn(_ sender: UIButton) {
+        UserApi.shared.loginWithKakaoAccount {(oauthToken, error) in
+            if let error = error {
+                print(error)
+                
+            }
+            else {
+                print("loginWithKakaoAccount() success.")
+                //do something
+                _ = oauthToken
+                
+                let vc = self.storyboard?.instantiateViewController(withIdentifier: "SecondVC")
+                self.present(vc!, animated: true, completion: nil)
+                
+            }
+            
+        }
+    }
+    
+    // 로그인에 성공한 경우 호출
+    func oauth20ConnectionDidFinishRequestACTokenWithAuthCode() {
+        print("Success login")
+        getInfo()
+    }
+    
+    // referesh token
+    func oauth20ConnectionDidFinishRequestACTokenWithRefreshToken() {
+        loginInstance?.accessToken
+    }
+    
+    // 로그아웃
+    func oauth20ConnectionDidFinishDeleteToken() {
+        print("log out")
+    }
+    
+    // 모든 error
+    func oauth20Connection(_ oauthConnection: NaverThirdPartyLoginConnection!, didFailWithError error: Error!) {
+        print("error = \(error.localizedDescription)")
+    }
+    
+    @IBAction func login(_ sender: Any) {
+        loginInstance?.requestThirdPartyLogin()
+    }
+    
+    @IBAction func logout(_ sender: Any) {
+        loginInstance?.requestDeleteToken()
+    }
+    
+    // RESTful API, id가져오기
+    func getInfo() {
+      guard let isValidAccessToken = loginInstance?.isValidAccessTokenExpireTimeNow() else { return }
+      
+      if !isValidAccessToken {
+        return
+      }
+      
+      guard let tokenType = loginInstance?.tokenType else { return }
+      guard let accessToken = loginInstance?.accessToken else { return }
+        
+      let urlStr = "https://openapi.naver.com/v1/nid/me"
+      let url = URL(string: urlStr)!
+      
+      let authorization = "\(tokenType) \(accessToken)"
+      
+      let req = AF.request(url, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: ["Authorization": authorization])
+      
+      req.responseJSON { response in
+        guard let result = response.value as? [String: Any] else { return }
+        guard let object = result["response"] as? [String: Any] else { return }
+        guard let name = object["name"] as? String else { return }
+        guard let email = object["email"] as? String else { return }
+        guard let id = object["id"] as? String else {return}
+        
+        print(email)
+        
+        self.nameLabel.text = "\(name)"
+        self.emailLabel.text = "\(email)"
+        self.id.text = "\(id)"
+      }
+    }
+    
+    
 }
 
